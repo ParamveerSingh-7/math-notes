1fba55cfe67facac4ac2b5f728773aef40201fc3
Paramveer Singh Matharu <115947762+ParamveerSingh-7@users.noreply.github.com>
2025-09-04 18:52:42 +0530

    Updated index.tsx

--- src/screen/home/index.tsx
+++ src/screen/home/index.tsx
@@ -1,8 +1,11 @@
 @@ .. @@
       const response = await axios({
         method: "post",
+      }
+      )
 -        url: `${import.meta.env.VITE_API_URL}/calculate`,
 +        url: "http://localhost:8900/calculate",
         data: {
           image: canvas.toDataURL("image/png"),
           dict_of_vars: dictOfVars,
+        }
