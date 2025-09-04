5cd498e6d9fbb290cdca9d1d5fb32f66604f4134
Paramveer Singh Matharu <115947762+ParamveerSingh-7@users.noreply.github.com>
2025-09-04 18:52:26 +0530

    Fix API configuration

---
+++ src/screen/home/index.tsx
@@ -0,0 +1,8 @@
+@@ .. @@
+      const response = await axios({
+        method: "post",
+-        url: `${import.meta.env.VITE_API_URL}/calculate`,
++        url: "http://localhost:8900/calculate",
+        data: {
+          image: canvas.toDataURL("image/png"),
+          dict_of_vars: dictOfVars,
